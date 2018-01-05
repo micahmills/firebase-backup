90e4344ff0061044e6000ef8869550b95c97119c
Micah Mills <micahmills@gmail.com>
2018-01-05 21:28:59 +0300

    delete messages that are older than a week old

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var https = require("https"),
   fs = require("fs");
 
+// Import Firebase Admin SDK
+var admin = require("firebase-admin");
+// Get a database reference to our blog
+var serviceAccount = require("./enjilchat-alpha-firebase-adminsdk-ro8js-944c945eb0.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://enjilchat-alpha.firebaseio.com"
+});
+
+
+// Get a database reference to our blog
+var db = admin.database();
+var ref = db.ref("messages");
+
 /* Set FIREBASE_URL and FIREBASE_SECRET */
 var FIREBASE_URL = "https://enjilchat-alpha.firebaseio.com";
 var FIREBASE_SECRET = "0dnX5eHlh29VNewzLWsYLStFe7PrsDCnqhLTssKd";
@@ -74,10 +89,10 @@ function combineBackups(data) {
 
 	var obj2 = JSON.parse(data);
 	
-	console.log('obj1');
-	console.log(JSON.stringify(obj1, null, 2));
-	console.log('obj2');
-	console.log(JSON.stringify(obj2, null, 2));
+	//console.log('obj1');
+	//console.log(JSON.stringify(obj1, null, 2));
+	//console.log('obj2');
+	//console.log(JSON.stringify(obj2, null, 2));
 
 	for (var chat in obj2) {
 		//if this chat exist in the old file 
@@ -86,20 +101,20 @@ function combineBackups(data) {
 			for (var message in obj2[chat]) {
 				//message in both objects move on
 				if (obj2[chat].hasOwnProperty(message) && obj1[chat].hasOwnProperty(message)) {
-					console.log('both message');
-					console.log(obj2[chat][message]);
+					//console.log('both message');
+					//console.log(obj2[chat][message]);
 				}
 				//message in new object but not old add it.
 				else if (obj2[chat].hasOwnProperty(message) && !obj1[chat].hasOwnProperty(message)) {
-					console.log('new message');
-					console.log(obj2[chat][message]);
+					//console.log('new message');
+					//console.log(obj2[chat][message]);
 					obj1[chat][message] = obj2[chat][message];
 				}
 			}
 		}
 		//if this chat doesn't exist in the old file
 		if (obj2.hasOwnProperty(chat) && !obj1.hasOwnProperty(chat)) { 
-			console.log(JSON.stringify(obj2[chat]));
+			//console.log(JSON.stringify(obj2[chat]));
 			obj1[chat] = obj2[chat];
 		}
 	}
@@ -108,12 +123,33 @@ function combineBackups(data) {
 
 function deleteOldMessages() {
 	console.log("Delete Old Messages from Firebase");
+	var now = Date.now();
+	var cutoff = now - 168 * 60 * 60 * 1000;
+	var updates = {};
+	  return ref.once('value', function(snapshot) {
+		// create a map with all children that need to be removed
+		var updates = {};
+		console.log("snapshot foreach");
+		snapshot.forEach(function(child) {
+			var chat = child.key
+			child.forEach(function(grandchild) {
+
+				if (grandchild.val().time <= cutoff) {
+					updates['/' + chat + '/' + grandchild.key] = null;
+					console.log(grandchild.key + " will be deleted");
+				}
+			})
+		});
+		// execute all updates in one go and return the result to end the function
+		console.log(updates);
+		return ref.update(updates);
+	  });
 };
 
 function init() {
   fetchData();
-  //   setInterval(fetchData, 86400000);//daily backup
-  setInterval(fetchData, 120000); //every 2 minutes backup for testing.
+     setInterval(fetchData, 86400000);//daily backup
+  //setInterval(fetchData, 120000); //every 2 minutes backup for testing.
 }
 
 init();
